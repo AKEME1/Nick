65bb8a1440b239e336bb454609379b99726fc794
Akeme <tedake36@gmail.com>
2023-09-21 14:55:11 +0300

    second up date

--- src/section/Services.jsx
+++ src/section/Services.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import { services } from "../constants";
+import ServiceCard from "../components/ServiceCard";
 
 const Services = () => {
-  return <div></div>;
+  return (
+    <section className="max-container flex justify-center flex-warp gap-9">
+      {services.map((services) => (
+        <ServiceCard />
+      ))}
+    </section>
+  );
 };
 
 export default Services;
