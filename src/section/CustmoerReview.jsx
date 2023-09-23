d354e065f2e55d6b6787ee823461da885e654906
Akeme <tedake36@gmail.com>
2023-09-23 16:55:56 +0300

    working with some part

--- src/section/CustmoerReview.jsx
+++ src/section/CustmoerReview.jsx
@@ -1,7 +1,18 @@
 import React from "react";
 
 const CustmoerReview = () => {
-  return <div></div>;
+  return (
+    <section className="max-container">
+      <h3 className=" font-palanquin text-center text-4xl font-bold">
+        What our <span className=" text-coral-red">Customers</span> Say?
+      </h3>
+      <p className=" info-text m-auto mt-4 max-w-lg text-center">
+        Hear genuine stories from our satisfied customer about their exceptional
+        experiences with us
+      </p>
+      <div className=" mt-24 flex flex-1 justify-evenly items-center max-lg:flex-col gap-14"></div>
+    </section>
+  );
 };
 
 export default CustmoerReview;
